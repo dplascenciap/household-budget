96f74eb025e8171584f0155c9c7123bc4f15fdd0
dplascenciap <dplascenciap@gmail.com>
2026-06-12 21:59:43 -0400

    fix: update version label to v1.5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ user, onSignOut }) {
     <header className="header">
       <div className="header-brand">
         🏠 Household Budget
-        <span>Plascencia-Frisbee v1.4</span>
+        <span>Plascencia-Frisbee v1.5</span>
       </div>
       <div className="header-right">
         <span className="header-user">👤 {firstName}</span>
